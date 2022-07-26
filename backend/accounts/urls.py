3227f1acdf6b6017a0978c856fc8a248c0b5864d
Austin <austin.jiuk.kim@mgmail.com>
2022-07-26 15:34:10 +0900

    main_api_ziweek

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from django.contrib import admin
+
 
 urlpatterns = [
-    path('', include('dj_rest_auth.urls')),
-    path('registration/', include('dj_rest_auth.registration.urls')),
+    path("", include("dj_rest_auth.urls")),
+    path("registration/", include("dj_rest_auth.registration.urls")),
 ]
--- backend/main/views.py
+++ backend/main/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
 # Create your views here.
+# from rest_framework import permissions
+from rest_framework import viewsets
+from .models import Products, Options, Orders, QnAs, Comments
+from .serializers import (
+    ProductSerializer,
+    OptionSerializer,
+    OrderSerializer,
+    QnASerializer,
+    CommentSerializer,
+)
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Products.objects.all()
+    serializer_class = ProductSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+class OptionViewSet(viewsets.ModelViewSet):
+    queryset = Options.objects.all()
+    serializer_class = OptionSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Orders.objects.all()
+    serializer_class = OrderSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+class QnAViewSet(viewsets.ModelViewSet):
+    queryset = QnAs.objects.all()
+    serializer_class = QnASerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comments.objects.all()
+    serializer_class = CommentSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
