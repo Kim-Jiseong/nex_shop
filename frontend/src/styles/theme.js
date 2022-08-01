2a4bddef484077d67b379801bbc1a22d894c9bf0
Kim Jiseong <35622664+Kim-Jiseong@users.noreply.github.com>
2022-08-01 14:48:27 +0900

    frontend setting

---
+++ frontend/src/styles/theme.js
@@ -0,0 +1,24 @@
+export const colors = {
+    white: "#f2f2f2",
+    whiteGray: "#E8E6E6",
+    midGray: "#989FA0",
+    yellow: "#ECB365",
+  };
+  
+  export const fonts = {
+    size: {
+      xs: "1.2rem",
+      small: "1.4rem",
+      middle: "1.6rem",
+      base: "1.8rem",
+      ml: "2.5rem",
+      large: "3rem",
+      title: "4.4rem",
+      title_main: "7.2rem",
+    },
+    weight: {
+      light: 200,
+      normal: 500,
+      bold: 700,
+    },
+  };
