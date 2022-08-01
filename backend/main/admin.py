ef387f4a218d4480c3ec11da7487c1001b3764ca
Kim Jiseong <35622664+Kim-Jiseong@users.noreply.github.com>
2022-08-01 19:51:30 +0900

    deploy settings

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Products, Options, Orders,QnAs,Comments 
 # Register your models here.
-admin.site.register(Products, Options, Orders,QnAs,Comments)
+admin.site.register(Products)
+admin.site.register(Options)
+admin.site.register(Orders)
+admin.site.register(QnAs)
+admin.site.register(Comments)
